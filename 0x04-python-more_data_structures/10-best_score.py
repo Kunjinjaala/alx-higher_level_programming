b7198e08c456883177d61a3a81eebeff2c991518
kunjinjaala <mhilmi9236@yahoo.com>
2024-01-11 22:00:33 -0800

    Python data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    max_key = max(a_dictionary, key=a_dictionary.get)
-    return max_key if a_dictionary else None
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
